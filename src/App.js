2cbc7c7a90320763821539a50969e93278a7f21f
AndreMadar95 <madarandre11@gmail.com>
2021-10-14 16:09:13 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Travis CI
+          Learn Travis 
         </a>
       </header>
     </div>
