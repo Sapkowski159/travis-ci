d38563f1617a9bfb4d5e510d9afa19fe53f8b834
AndreMadar95 <madarandre11@gmail.com>
2021-10-14 16:33:30 -0400

    Added only for this branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Travis dev
+          Learn Travis dev ONLY
         </a>
       </header>
     </div>
